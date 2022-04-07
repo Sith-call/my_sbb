843b49f5b267adb545b8275ef13aaf9845b93707
wsChoe <chldntjr0425@gmail.com>
2022-04-07 16:06:39 +0900

    chore:check session fail

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -17,19 +18,16 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
-	@Autowired
-	private AnswerRepository answerRepository;
-
+	//@Transactional
 	@Test
 	void contextLoads() {
-		Optional<Question> oq = this.questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		
-		List<Answer> answerList = q.getAnswerList();
-		
-		assertEquals(1,answerList.size());
-		assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
-	}
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
